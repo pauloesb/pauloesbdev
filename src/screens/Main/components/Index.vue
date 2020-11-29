698318f7c7eb105df746562d9aa75c36c935a41a
Paulo Borba <pauloesb@gmail.com>
2020-11-29 16:35:54 -0300

    Create component structure

---
+++ src/screens/Main/components/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class MainIndex extends Vue {}
+</script>
